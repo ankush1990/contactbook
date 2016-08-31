6fb9b7cdd96a2831dff6ae4a31e8ee5de8ef3bf9
ankush1990 <ank_ankush@yahoo.co.in>
2016-08-31 18:43:12 +0530

    20160831

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -45,24 +45,13 @@ angular.module('starter.controllers', [])
 	$timeout( function(){ $scope.getContactList(); }, 3000);
 	
 	$scope.getContactList = function() {
-		 $scope.phoneContacts = [];
-
-		function onSuccess(contacts) {
-		  for (var i = 0; i < contacts.length; i++) {
-			var contact = contacts[i];
-			$scope.phoneContacts.push(contact);
-		  }
-		};
-	
-		function onError(contactError) {
-		  alert(contactError);
-		};
-	
-		var options = {};
-		options.multiple = true;
-	
-		$cordovaContacts.find(options).then(onSuccess, onError);
-	  
+		 $scope.getContactList = function() {
+			$cordovaContacts.find({filter: ''}).then(function(result) {
+				$scope.contacts = result;
+			}, function(error) {
+				console.log("ERROR: " + error);
+			});
+		}
 	}
 })
 
